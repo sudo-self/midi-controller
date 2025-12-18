6d06278c1cbf6ff38b99936b9b2459addbc60f7a
sudo-self <sudo-self@proton.me>
2025-12-17 23:53:28 -0700

    Update MidiController.tsx

--- components/MidiController.tsx
+++ components/MidiController.tsx
@@ -772,7 +772,7 @@ const MidiController: React.FC = () => {
               </a>
               <div className="text-zinc-600 text-sm">•</div>
               <div className="text-zinc-400 text-sm">
-                <span className="text-cyan-400">♫♪♫♪♫♪</span><span className="text-pink-400">♫♪♫♪</span>
+                <span className="text-cyan-400">♫♪♫</span> JR <span className="text-pink-400">♫♪♫</span>
               </div>
             </div>
           </div>
